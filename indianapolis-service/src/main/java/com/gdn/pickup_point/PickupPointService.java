a01ae6e79f48d5e9709eb84fa078b8b8969834ee
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2018-07-04 10:43:29 +0700

    - removing pickup point repository and service

--- indianapolis-service/src/main/java/com/gdn/pickup_point/PickupPointService.java
+++
@@ -1,7 +0,0 @@
-package com.gdn.pickup_point;
-
-import com.gdn.entity.PickupPoint;
-
-public interface PickupPointService {
-    PickupPoint findByPickupAddressOrLatitudeOrLongitude(String address, double latitude, double longitude);
-}
