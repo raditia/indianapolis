15137125110b557ae81a3026e9bbd40eb0358361
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2018-08-03 11:16:04 +0700

    - removed every util in indianapolis service test

--- indianapolis-service/src/test/java/com/gdn/util/PickupChoiceRequestUtil.java
+++
@@ -1,9 +0,0 @@
-package com.gdn.util;
-
-import com.gdn.request.PickupChoiceRequest;
-
-public class PickupChoiceRequestUtil {
-    public static PickupChoiceRequest pickupChoiceRequestCompleteAttribute = PickupChoiceRequest.builder()
-            .recommendationResultId("id")
-            .build();
-}
